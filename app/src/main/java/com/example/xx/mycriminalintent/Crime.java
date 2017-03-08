5b8d5c679cf555ac3e3b5e3b343ccc16ea3b25b5
dashgxx <g382070527@163.com>
2017-03-09 00:09:45 +0800

    new: landscape layout detail and solve

--- app/src/main/java/com/example/xx/mycriminalintent/Crime.java
+++ app/src/main/java/com/example/xx/mycriminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.example.xx.mycriminalintent;
 
+import java.util.Date;
 import java.util.StringTokenizer;
 import java.util.UUID;
 
@@ -10,10 +11,13 @@ import java.util.UUID;
 public class Crime {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public Crime()
     {
         mId=UUID.randomUUID();
+        mDate=new Date();
     }
 
     public String getTitle() {
@@ -27,5 +31,21 @@ public class Crime {
     public UUID getId() {
 
         return mId;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
     }
 }
--- app/src/main/java/com/example/xx/mycriminalintent/CrimeFragment.java
+++ app/src/main/java/com/example/xx/mycriminalintent/CrimeFragment.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 /**
@@ -17,6 +21,8 @@ import android.widget.EditText;
 public class CrimeFragment extends Fragment {
     private Crime mCrime;
     private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -47,6 +53,19 @@ public class CrimeFragment extends Fragment {
             }
         });
 
+        mDateButton=(Button)v.findViewById(R.id.crime_date);
+        final CharSequence dateString = DateFormat.format("E,MMM dd,yyyy", mCrime.getDate());
+        mDateButton.setText(dateString);
+        mDateButton.setEnabled(false);
+
+        mSolvedCheckBox=(CheckBox)v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                mCrime.setSolved(b);
+            }
+        });
+        
         return v;
         //return super.onCreateView(inflater, container, savedInstanceState);
     }
